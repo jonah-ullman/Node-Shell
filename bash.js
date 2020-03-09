d0212ba2a2f4983a9d79a60b546c7537a98c1e06
Jonah Ullman <jonahmullman@gmail.com>
2020-03-09 12:16:48 -0500

    added pwd/ls functionality

--- bash.js
+++ bash.js
@@ -1,9 +1,15 @@
+const { pwd } = require('./pwd');
+const { ls } = require('./ls');
+
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', (data) => {
-	const cmd = data.toString().trim();
-	if (cmd === 'pwd') {
-		process.stdout.write(`Current directory: ${process.cwd()}`);
-	}
-	process.stdout.write('\nprompt > ');
+process.stdin.on('data', data => {
+  const cmd = data.toString().trim();
+  if (cmd === 'pwd') {
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
+  }
+
+  process.stdout.write('\nprompt > ');
 });
---
+++ pwd.js
@@ -0,0 +1,7 @@
+function pwd() {
+process.stdout.write(`Current directory: ${process.cwd()}`);
+}
+
+module.exports = {
+  pwd
+}
